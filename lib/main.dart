65e29ad3a54b1ac1e7dd3e47e1cf32b026ab6950
Bharath Biju <bharathinfoclg@gmail.com>
2020-03-22 23:00:55 +0530

    splash screen "welcome to cg"

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,66 @@ import 'package:myapp/clg_details.dart';
 import 'package:myapp/course_details.dart';
 import 'second_page.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: 'title',
-    home: new MyApp(),
-  ));
+ 
+void main() => runApp(SplashHandler());
+
+class SplashHandler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcom to CG.",
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () async {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyApp()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Welcom to ",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontFamily: "Poppins-Bold",
+                  fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "CGs",
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 50,
+                fontFamily: "Poppins-Bold",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
